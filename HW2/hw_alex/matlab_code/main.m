2fa3d563133aadf9e736b1019c5a77085be34ba4
Alexander Shender <aka.sova@gmail.com>
2019-06-01 21:56:09 +0300

    some work towards q.2

--- HW2/hw_alex/matlab_code/main.m
+++ HW2/hw_alex/matlab_code/main.m
@@ -365,31 +365,31 @@ nearest_dog = find_closest_neighbor(dogs_PC_scores, wolf_PC_score); % 8
 %%  PCA good lesson
 
 % 
-% rng 'default'
-% M = 7; % Number of observations
-% N = 5; % Number of variables observed
-% X = rand(M,N);
-% % De-mean
-% X = X - mean(X);
-% % Do the PCA
-% [coeff,score,latent] = pca(X);
-% % Calculate eigenvalues and eigenvectors of the covariance matrix
-% covarianceMatrix = cov(X);
-% [V,D] = eig(covarianceMatrix);
-% % "coeff" are the principal component vectors. These are the eigenvectors of the covariance matrix. Compare ...
-% coeff
-% V
-% % Multiply the original data by the principal component vectors to get the projections of the original data on the
-% % principal component vector space. This is also the output "score". Compare ...
-% dataInPrincipalComponentSpace = X*coeff
-% score
-% % The columns of X*coeff are orthogonal to each other. This is shown with ...
-% corrcoef(dataInPrincipalComponentSpace)
-% % The variances of these vectors are the eigenvalues of the covariance matrix, and are also the output "latent". Compare
-% % these three outputs
-% var(dataInPrincipalComponentSpace)'
-% latent
-% sort(diag(D),'descend')
+rng 'default'
+M = 7; % Number of observations
+N = 5; % Number of variables observed
+X = rand(M,N);
+% De-mean
+X = X - mean(X);
+% Do the PCA
+[coeff,score,latent] = pca(X);
+% Calculate eigenvalues and eigenvectors of the covariance matrix
+covarianceMatrix = cov(X);
+[V,D] = eig(covarianceMatrix);
+% "coeff" are the principal component vectors. These are the eigenvectors of the covariance matrix. Compare ...
+coeff
+V
+% Multiply the original data by the principal component vectors to get the projections of the original data on the
+% principal component vector space. This is also the output "score". Compare ...
+dataInPrincipalComponentSpace = X*coeff
+score
+% The columns of X*coeff are orthogonal to each other. This is shown with ...
+corrcoef(dataInPrincipalComponentSpace)
+% The variances of these vectors are the eigenvalues of the covariance matrix, and are also the output "latent". Compare
+% these three outputs
+var(dataInPrincipalComponentSpace)'
+latent
+sort(diag(D),'descend')
 
 
 
