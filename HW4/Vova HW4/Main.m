1db5e77bc46c9da3cd03aea981a5ff1fbe430019
Vladimir Tchuiev <rmcf126@gmail.com>
2019-06-20 11:34:09 +0300

    As an idiot I took the test set for training and I wondered how I should take 25 eigenvectors if there are only 20 images... Fixed it, still need to use the k-neighbours function

--- HW4/Vova HW4/Main.m
+++ HW4/Vova HW4/Main.m
@@ -2,13 +2,14 @@ clear
 close all
 clc
 
-addpath('eigenFaces');
+addpath(genpath('eigenFaces'));
 run('readYaleFaces.m');
-image = zeros( 243 , 320 , 20 );
+test_image = zeros( 243 , 320 , 20 );
+image = reshape( A , [ 243 , 320 , 150 ]);
 
 for i = 1:20 % The image numbering is bad programing; put all images in a single variable
     testFileName = ['image' num2str(i)];
-    image( : , : , i ) = double( eval ( testFileName ) );
+    test_image( : , : , i ) = double( eval ( testFileName ) );
 end
 
 %%
@@ -25,9 +26,9 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 average_image = zeros( 243 , 320 );
-for i = 1 : 1 : 20
+for i = 1 : 1 : 150
     
-    average_image = average_image + double( image( : , : , i ) ) / 20;
+    average_image = average_image + double( image( : , : , i ) ) / 150;
     
 end
 
@@ -37,53 +38,105 @@ imshow( uint8( average_image ) );
 % Subtract mean image (1.b)
 
 subtracted_image = zeros( 243 , 320 );
-for i = 1 : 1 : 20
+for i = 1 : 1 : 150
     
     subtracted_image( : , : , i ) = ( image( : , : , i ) - average_image );
     
 end
 
-figure(102)
-imshow( uint8( subtracted_image( : , : , 3 ) + 126 ) );
-
 % Compute eigenvectors and report first 5 eigen-faces (2)
 
-eimage = eigenimages( subtracted_image / sqrt(20) , 5 );
+eimage = eigenimages( subtracted_image / sqrt(150) , 5 );
 
 for i = 1 : 1 : 5
     figure(200+i)
-    imshow( uint8( eimage( : , : , i ) ) + 126 )
+    step_image = eimage( : , : , i );
+    normalized_image = step_image / max( step_image(:) ) * 126; 
+    imshow( uint8( normalized_image + 126 ) )
 end
 
 % Display and compute the representation error for the training images (3)
 
-W_size = 20;
-Dr_RMSE = zeros( W_size , 1 );
-RMSE = zeros( W_size , 1 );
+W_size = 25;
+Dr_RMSE_squared = zeros( 150 , 1 );
+RMSE_squared = zeros( 150 , 1 );
 
-eimage_large = eigenimages( subtracted_image / sqrt(20) , W_size );
+eimage_large = eigenimages( subtracted_image / sqrt(150) , W_size );
 W_matrix = reshape( eimage_large , 243 * 320 , W_size );
 
-for i = 1 : 1 : 20
+for i = 1 : 1 : 150
     
     y_rep = W_matrix' * reshape( subtracted_image( : , : , i ) , 243 * 320 , 1 );
-    y_rep = y_rep / (sum(y_rep));
     reconstructed = reshape( average_image , [ 320 * 243 , 1] ) + W_matrix * y_rep;
-%     reconstructed = ( reconstructed ) /...
-%         ( max( reconstructed(:) ) ) * 255; 
-    reconstructed = ( reconstructed - min( reconstructed(:) ) ) /...
-        ( max( reconstructed(:) ) - min( reconstructed(:) ) ) * 255; 
     reconstructed = reshape( reconstructed , [ 243 , 320 ] );
-    figure(301)
-    imshow( uint8( reconstructed )  )
-    figure(302)
-    imshow( uint8( image( : , : , i ) ) )
-    figure(303)
-    imshow( uint8( reshape( W_matrix * y_rep , [ 243 , 320 ] ) ) )
+%     figure(301)
+%     imshow( uint8( reconstructed )  )
+%     figure(302)
+%     imshow( uint8( image( : , : , i ) ) )
+%     figure(303)
+%     imshow( uint8( reshape( W_matrix * y_rep , [ 243 , 320 ] ) ) )
+    
+    normalized_training_image = image( : , : , i ) / ( max( image( : , : , i ) ) - min( image( : , : , i ) ) );
+    normalized_reconstruction = reconstructed / ( max( reconstructed ) - min( reconstructed ) );
+    
+    Dr_RMSE_squared( i ) = sum(sum( ( normalized_reconstruction...
+        - normalized_training_image ).^2 )) / ( 243 * 320 * 150 );
+    
+    RMSE_squared( i ) = sum(sum( ( reconstructed / 255 ...
+        - image( : , : , i ) / 255 ).^2 )) / ( 243 * 320 * 150 );
     
 end
 
+Dr_RMSE = Dr_RMSE_squared.^.5;
+RMSE = RMSE_squared.^.5;
+
+figure(310)
+hold on
+plot(RMSE, 'r')
+plot(Dr_RMSE)
+xlabel( 'Image number' )
+ylabel( 'Error' )
+legend( 'RMSE' , 'Dynamic range RMSE')
+
 % Compute the representation error for the test images. Classify the test images and report error rate (4)
 
+subtracted_test_image = zeros( 243 , 320 , 20 );
+for i = 1 : 1 : 20
+    subtracted_test_image( : , : , i ) = test_image( : , : , i ) - average_image;
+end
+
+for i = 1 : 1 : 20
+    
+    y_rep = W_matrix' * reshape( subtracted_test_image( : , : , i ) , 243 * 320 , 1 );
+    reconstructed = reshape( average_image , [ 320 * 243 , 1] ) + W_matrix * y_rep;
+    reconstructed = reshape( reconstructed , [ 243 , 320 ] );
+    figure(401)
+    imshow( uint8( reconstructed )  )
+    figure(402)
+    imshow( uint8( test_image( : , : , i ) ) )
+    figure(403)
+    imshow( uint8( reshape( W_matrix * y_rep , [ 243 , 320 ] ) ) )
+    
+    normalized_test_image = test_image( : , : , i ) / ( max( test_image( : , : , i ) ) - min( test_image( : , : , i ) ) );
+    normalized_reconstruction = reconstructed / ( max( reconstructed ) - min( reconstructed ) );
+    
+    Dr_RMSE_squared( i ) = sum(sum( ( normalized_reconstruction...
+        - normalized_test_image ).^2 )) / ( 243 * 320 * 150 );
+    
+    RMSE_squared( i ) = sum(sum( ( reconstructed / 255 ...
+        - test_image( : , : , i ) / 255 ).^2 )) / ( 243 * 320 * 150 );
+    
+end
+
+Dr_RMSE = Dr_RMSE_squared.^.5;
+RMSE = RMSE_squared.^.5;
+
+figure(410)
+hold on
+plot(RMSE, 'r')
+plot(Dr_RMSE)
+xlabel( 'Image number' )
+ylabel( 'Error' )
+legend( 'RMSE' , 'Dynamic range RMSE')
 
 return;
--- HW4/Vova HW4/eigenimages.m
+++ HW4/Vova HW4/eigenimages.m
@@ -1,4 +1,4 @@
-function [ sorted_eimage ] = eigenimages( image , r )
+function [ normalized_W_matrix ] = eigenimages( image , r )
 
 % This function creates a sorted array of eigenimages based on the highest
 % eigenvalues. It gets an array of grayscale images and outputs an array of
@@ -18,10 +18,15 @@ sorted_evec = A_matrix * eigenvectors;
 sorted_evec = sorted_evec( : , idx );
 
 sorted_eimage = reshape( sorted_evec , [ image_size( 1 ) , image_size( 2 ) , image_size( 3 ) ] );
-if r <= image_size( 3 )
+if r >= image_size( 3 )
     r = image_size( 3 );
 end
 sorted_eimage = sorted_eimage( : , : , 1 : r );
+W_matrix = reshape( sorted_eimage , [ image_size( 1 ) * image_size( 2 ) , r ] );
+WtW_matrix = W_matrix' * W_matrix;
+Normalizer = 1 ./ diag( WtW_matrix ).^.5;
+normalized_W_matrix = W_matrix * diag( Normalizer );
+normalized_W_matrix = reshape( normalized_W_matrix , [ image_size( 1 ) , image_size( 2 ) , r ] );
 % for i = 1 : 1 : r
 %     part_sorted_eimage = sorted_eimage( : , : , i );
 %     sorted_eimage( : , : , i ) = ( sorted_eimage( : , : , i ) -...
